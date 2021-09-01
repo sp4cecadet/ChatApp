4cced9d8d3fbf5a35ee9e425821bd06c1ad6d78e
sp4cecadet <iventus2020@gmail.com>
2021-09-02 00:44:52 +0300

    avatar depicturing if set

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -5,7 +5,7 @@ import "./Avatar.scss";
 
 const Avatar = ({ user }) => {
   if (user.avatar) {
-    return <img className="avatar" src={user.avatar} alt={user.fullname} />;
+    return <img className="avatar" src={user.avatar.url} alt={user.fullname} />;
   } else {
     const { color, colorLighten } = generateAvatar(user._id);
     const firstChar = user.fullname[0].toUpperCase();
